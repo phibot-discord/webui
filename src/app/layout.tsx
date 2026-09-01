b2bce805724ced4b5b29f011d5a973c6660571c7
YueMiyuki <76854136+YueMiyuki@users.noreply.github.com>
2026-09-01 17:57:07 +0800

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Script from "next/script";
@@ -64,6 +65,7 @@ export default async function RootLayout({
 						<SiteFooter m={m} />
 					</div>
 				</I18nProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
